e2199d60e5a0d97e5ede7a45027ddc36ff85518f
Yann Hippert <yann.hippert@hotmail.com>
2023-04-22 13:46:33 +0200

    Fixed ContextMenu going under ControlBar

--- src/components/TableContextMenu/TableContextMenu.tsx
+++ src/components/TableContextMenu/TableContextMenu.tsx
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux';
 import { AppState, togglePlaylistSong } from '../../redux/slices/playlistSlice';
 
 import './TableContextMenu.scss';
+import { Playlist } from '../../interfaces/playlist';
+import useWindowDimensions from './useWindowDimensions';
 
 interface TableContextMenuProps {
     songKey: string;
@@ -14,18 +16,23 @@ interface TableContextMenuProps {
 
 const TableContextMenu = ({ songKey, isVisible, x, y }: TableContextMenuProps) => {
     const dispatch = useDispatch();
-    const playlists = useSelector(({ store }: { store: AppState }) => store.playlists.filter((p) => p.isPersonal));
+    const playlists = useSelector(({ store }: { store: AppState }) => store.playlists.filter(({ isPersonal }: Playlist) => isPersonal));
+    const { height } = useWindowDimensions();
 
     const handleContextMenuItemClick = (playlistKey: string) => {
         dispatch(togglePlaylistSong({ playlistKey, songKey }));
     };
 
+    const menuDirection = () => {
+        return y > height / 2 ? 'up-menu' : 'down-menu';
+    };
+
     if (!isVisible) return <></>;
 
     return (
-        <div className="table-context-menu" style={{ left: `${x}px`, top: `${y}px` }}>
+        <div className={`table-context-menu ${menuDirection()}`} style={{ left: `${x}px`, top: `${y}px` }}>
             <p className="context-menu-header">Add to playlist</p>
-            <ul className="context-menu-options" style={{ left: `${x}px`, top: `${y}px` }}>
+            <ul className="context-menu-options">
                 {playlists.map((playlist) => (
                     <li className="context-menu-item" key={playlist.key} onClick={() => handleContextMenuItemClick(playlist.key)}>
                         {playlist.name}
